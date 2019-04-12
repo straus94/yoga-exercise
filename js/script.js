fd4b3f5f2e1e083846402c0a27ec7a622fd95320
straus <straus94@gmail.com>
2019-04-12 13:08:56 +0200

    add timer on page

--- js/script.js
+++ js/script.js
@@ -35,5 +35,60 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+       //timer
+       let deadLine = '2019-04-20';
+
+       function getTimeRemaining(endTime) {
+           let t = Date.parse(deadLine) - Date.parse(new Date());
+           let seconds = Math.floor((t/1000) % 60),
+               minutes = Math.floor((t / 1000 / 60) % 60),
+               hours = Math.floor((t / (1000 * 60 * 60)));
+   
+           return {
+               'total' : t,
+               'hours' : hours,
+               'minutes' : minutes,
+               'seconds' : seconds
+           };
+       }
+   
+       function setClock(id, endTime) {
+           let timer = document.getElementById(id),
+               hours = timer.querySelector('.hours'),
+               minutes = timer.querySelector('.minutes'),
+               seconds = timer.querySelector('.seconds'),
+               timeInterval = setInterval(updateClock, 1000);
+   
+           
+           function updateClock() {
+               let t = getTimeRemaining(endTime);
+               if (t.hours < 10) {
+                   hours.textContent = "0" + t.hours;
+               } else {
+                   hours.textContent = t.hours;
+               }
+               
+               if (t.minutes < 10) {
+                   minutes.textContent = "0" + t.minutes;
+               } else {
+                   minutes.textContent = t.minutes;
+               }
+               
+   
+               if (t.seconds < 10) {
+                   seconds.textContent = "0" + t.seconds;
+               } else {
+                   seconds.textContent = t.seconds;
+               }
+               
+   
+               if (t.total < 0) {
+                   clearInterval(timeInterval);
+               }
+           }
+       }
+   
+       setClock('timer', deadLine);
+
 });
 
